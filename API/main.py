1cdfd0d18e32c6bfe80e230dca35831e17fe6a62
brybryboi <brybryboi215@gmail.com>
2025-04-13 01:48:19 -0400

    api functionality

--- API/main.py
+++ API/main.py
@@ -1,10 +1,46 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
+from pydantic import BaseModel
 
 app = FastAPI()
 client = MongoClient('mongodb+srv://brybryboi215:C3B32haPb6huuznE@cluster0.cfzxa0f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 db = client.scores
 
-@app.get("/")
-async def hello():
-    return {"message" : "hello world"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5173", "http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Event(BaseModel):
+    name : str
+    eventId : int
+
+class Score(BaseModel):
+    name : str
+    score : float
+    event : Event
+    ranking : int
+
+@app.post("/api/submit_score")
+async def submit_score(data: Score):
+    db.scores.insert_one(data.dict())
+    return {"message" : "Score Submitted Successfully!"}
+
+@app.get("/api/{event}/{name}")
+async def get_athlete(event : str, name : str):
+    try:
+        result = db.scores.find_one({
+            "name": name,
+            "event.name": event
+        })
+        if not result:
+            return "get request not good"
+        result["_id"] = str(result["_id"])
+        return result
+    except Exception as e:
+        print("🔥 Error:", e)
+        return e
--- ReactApp/live-scoring/src/App.jsx
+++ ReactApp/live-scoring/src/App.jsx
@@ -4,15 +4,64 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  //C3B32haPb6huuznE mongo pw
+  const comps = ["a", "b", "c"]
+  const [name, setName] = useState(comps[0])
+  const [score, setScore] = useState("")
+  const [event, setEvent] = useState("")
+  const [ranking, setRanking] = useState(0)
 
-  return (
-    
+  async function getScores() {
+    try {
+      const response = await fetch(`http://localhost:8000/api/${event}/${name}`);
+      if (!response.ok) {
+        throw new Error("Error with getting athlete score")
+      }
+      const data = await response.json();
+      console.log(data)
+    }
+    catch(error) {
+      console.log(error.message);
+    }
+  }
+
+  async function submitScore() {
+    try {
+      const response = await fetch('http://localhost:8000/api/submit_score', {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify({
+          name: name,
+          score: score,
+          event : {
+            name : "adv_cq",
+            eventId : 1,
+          },
+          ranking : 1
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Error with fetch");
+      }
+      console.log("success")
+    }
+    catch(error) {
+      console.log(error.message)
+    }
+  }
+
+  return (  
     <div id="center">
-      <input type="text" placeholder="Enter Score Here:"/>
+      <h2>Enter Score for {name}</h2>
+      <input type="text" placeholder="Enter Score Here:" value={score} onChange={(e) => setScore(e.target.value)}/>
       <br></br>
-      <button type="submit">Submit Score</button>
+      <button onClick={submitScore}>Submit Score</button>
+
+      <h2>Get score for</h2>
+      <input type="text" placeholder="Name" onChange={(e) => setName(e.target.value)}/>
+      <input type="text" placeholder="Event" onChange={(e) => setEvent(e.target.value)}/>
+      <button onClick={getScores}>get</button>
     </div>
   )
 }
